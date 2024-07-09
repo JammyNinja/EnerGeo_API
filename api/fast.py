ab63a2c15c0fcbdc8e371635d776d1fe9851c0bf
Louis Auger <jammyninja95@gmail.com>
2024-07-09 15:18:40 +0100

    added print statements, and smooth handling if elexon api goes down. also added geodict endpoint

--- api/fast.py
+++ api/fast.py
@@ -6,12 +6,11 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse #returning regional image
 
-from api.geo import geo_test_file
+from api.geo import geo_test_file, carbon_intensity_live_geodict
 from api.current import get_current_data_as_elements
 
 app = FastAPI()
 
-from fastapi.middleware.cors import CORSMiddleware
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -20,10 +19,13 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+this_folder = os.path.dirname(__file__)
+path_to_data = os.path.join(this_folder, "..", "data")
+
 # Define a root `/` endpoint
 @app.get('/')
 def index():
-    return {'ok': True, "docs" : "/docs"}
+    return {'ok': True, "docs to see endpoints" : "/docs"}
 
 @app.get('/test')
 def get_current_energy_production():
@@ -32,8 +34,6 @@ def get_current_energy_production():
     """
 
     filename = "test_current.json"
-    this_folder = os.path.dirname(__file__)
-    path_to_data = os.path.join(this_folder, "..", "data")
     filepath = os.path.join(path_to_data, filename)
 
     #load json from file
@@ -53,8 +53,6 @@ def get_current_energy_production():
 @app.get('/historical')
 def query_local_csv(year=None):
     filename = "df_fuel_ckan.csv"
-    this_folder = os.path.dirname(__file__)
-    path_to_data = os.path.join(this_folder, "..", "data")
     filepath = os.path.join(path_to_data, filename)
 
     df = pd.read_csv(filepath)
@@ -104,13 +102,20 @@ def query_elexon_api():
 @app.get('/current')
 def get_current():
     """
-    query the elexon API
-    returns dictionary specially formatted for front-end display
+        query the elexon API
+        returns dictionary specially formatted for front-end display
     """
     url = "https://data.elexon.co.uk/bmrs/api/v1/generation/actual/per-type/day-total?format=json"
-    response = requests.get(url).json()
-    dict_out = get_current_data_as_elements(response)
-    return dict_out
+    print("requesting url:", url)
+    response = requests.get(url)
+
+    if response.status_code == 200: #use '200' to simulate it going down
+        print("Getting current cardbon intensity data from the url")
+        output = get_current_data_as_elements(response.json())
+    else:
+        output = f"""Elexon API did not respond with code 200.\nResponse code was: {response.status_code}\nurl: {url}""".split('\n')
+
+    return output
 
 def cache_locally():
     """code to save locally, untested for compatibility with other functions"""
@@ -123,16 +128,21 @@ def cache_locally():
     # with open(filepath, 'r') as file_cache_in:
     #     response = json.load(file_cache_in)
     #     print(response)
-
     return None
-  
-@app.get('/geo_test')
+
+@app.get('/geo/static_image')
 async def geo_test():
     geo_filepath = geo_test_file()
+    print("serving regional carbon intensity data from local", geo_filepath)
     return FileResponse(geo_filepath)
+
+@app.get('/geo/intensity_geodict')
+def geo_carbon_as_geo_dict():
+    return carbon_intensity_live_geodict()
 
 if __name__ == "__main__":
     # exported()
     # query_elexon_api()
     # geo_test()
     # print(get_current())
+    pass
