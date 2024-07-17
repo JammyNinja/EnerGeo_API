4fbd9e5a3b0a446cf9365951b9e4d68a611467d2
Louis Auger <jammyninja95@gmail.com>
2024-07-17 17:04:56 +0100

    adapted generate image tool to respond with a dictionary containing image url and path

--- api/agent/tools/image.py
+++ api/agent/tools/image.py
@@ -18,6 +18,7 @@ def image_downloader(image_url: str | None) -> str:
         return "Could not download image from URL."
     unique_id: uuid.UUID = uuid.uuid4()
     image_path = IMAGE_DIRECTORY / f"{unique_id}.png"
+    print("image generator path", image_path)
     with open(image_path, "wb") as file:
         file.write(response.content)
     return str(image_path)
@@ -29,9 +30,27 @@ class GenerateImageInput(BaseModel):
 
 
 #original
+# @tool("generate_image", args_schema=GenerateImageInput)
+# def generate_image(image_description: str) -> str:
+#     """Generate an image based on a detailed description."""
+#     response = CLIENT.images.generate(
+#         model="dall-e-3",
+#         prompt=image_description,
+#         size="1024x1024",
+#         quality="standard",  # standard or hd
+#         n=1,
+#     )
+#     image_url = response.data[0].url
+#     print(image_url)
+#     # return image_downloader(image_url)
+#     return image_url
+
+#trying to return image path and url
 @tool("generate_image", args_schema=GenerateImageInput)
 def generate_image(image_description: str) -> str:
-    """Generate an image based on a detailed description."""
+    """Generate a dictionary that contains both the url and the path of a
+        generated image based on a detailed description.
+    """
     response = CLIENT.images.generate(
         model="dall-e-3",
         prompt=image_description,
@@ -40,9 +59,16 @@ def generate_image(image_description: str) -> str:
         n=1,
     )
     image_url = response.data[0].url
+
     print(image_url)
-    #return image_downloader(image_url)
-    return image_url
+    image_path = image_downloader(image_url)
+    print(image_path)
+
+    return {
+        "image_url" : image_url,
+        "image_path": image_path
+    }
+
 
 
 #tool attempt
